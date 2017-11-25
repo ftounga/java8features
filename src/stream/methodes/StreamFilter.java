a3853a51d3450d70ebb0bc59ac4af95cbbceb442
ftounga <ntounga@gmail.com>
2017-11-25 14:18:43 +0100

    skip elements

--- src/stream/methodes/StreamFilter.java
+++ src/stream/methodes/StreamFilter.java
@@ -24,6 +24,11 @@ public class StreamFilter {
 		 */
 		List<Integer> pairNumberUniqueElement =list.stream().filter(n -> n%2 ==0).distinct().collect(Collectors.toList());
 		System.out.println("Les liste des nombres pairs est: " +pairNumberUniqueElement.toString());
+		/**
+		 * Skip,Filter with a predicate and select unique element
+		 */
+		List<Integer> skipPairNumberUniqueElement =list.stream().filter(n -> n%2 ==0).distinct().skip(2).collect(Collectors.toList());
+		System.out.println("Les liste des nombres pairs est: " +skipPairNumberUniqueElement.toString());
 	}
 
 }
