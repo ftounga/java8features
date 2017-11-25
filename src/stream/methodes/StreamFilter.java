2dbbc2ab3414b5fd91df06f296fbde38fd0196bd
ftounga <ntounga@gmail.com>
2017-11-25 14:05:05 +0100

    Filter with a predicate

--- src/stream/methodes/StreamFilter.java
+++ src/stream/methodes/StreamFilter.java
@@ -1,10 +1,24 @@
 package stream.methodes;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StreamFilter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,8,4,1,9,10,11,12);
+		
+		
+		/**
+		 * Filter with a predicate n -> n % 2 == 0 for pairs numbers
+		 * ***/
+		List<Integer> pairNumber =list.stream().filter(n -> n%2 ==0).collect(Collectors.toList());
+		System.out.println("Les liste des nombre pair est: " +pairNumber.toString());
+		
 	}
 
 }
