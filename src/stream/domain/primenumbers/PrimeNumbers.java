e3f3b8e600278ac9aa48db8e389ee473c4470fbc
F-TOUNGA <F-TOUNGA@PO442105.axa-gie.axagie.lan>
2017-11-27 17:01:50 +0100

    Prime numbers

---
+++ src/stream/domain/primenumbers/PrimeNumbers.java
@@ -0,0 +1,26 @@
+package stream.domain.primenumbers;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("La liste des nombres premiers et non premiers allant qu'à 50" + partitionPrimes(50));
+		
+	}
+	
+	static boolean  isPrime(int candidate) {
+		int candidateRoot = (int) Math.sqrt((double) candidate);
+		return IntStream.rangeClosed(2, candidateRoot).noneMatch(i -> candidate % i ==0);
+	}
+	
+	static Map<Boolean, List<Integer>> partitionPrimes(int n){
+		return IntStream.rangeClosed(2, n).boxed().collect(Collectors.partitioningBy(candidate -> isPrime(candidate)));
+	}
+
+}
