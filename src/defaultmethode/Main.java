c841ee435aa8ec6e7167b75bb5ecbb6d337fae30
F-TOUNGA <F-TOUNGA@PO442105.axa-gie.axagie.lan>
2017-12-06 14:35:36 +0100

    Compose interface

---
+++ src/defaultmethode/Main.java
@@ -0,0 +1,14 @@
+package defaultmethode;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Monster m = new Monster();
+		m.rotateBy(180);
+		m.moveVertically(10);
+		
+		System.out.println(" This pattern help to compose the interface");
+	}
+
+}
